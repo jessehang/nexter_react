e7928f5eaa4dfa4deed113a082e55e78dcdd023f
Jesse Hang <hangtimee@gmail.com>
2019-12-11 21:24:03 -0800

    [header] completed header content

--- public/src/components/Header.js
+++ public/src/components/Header.js
@@ -2,6 +2,10 @@ import React from 'react';
 import styles from '../styles/header.scss';
 import font from '../styles/typography.scss';
 import logo from '../../../img/logo.png';
+import bbc from '../../../img/logo-bbc.png';
+import forbes from '../../../img/logo-forbes.png';
+import techcrunch from '../../../img/logo-techcrunch.png';
+import bi from '../../../img/logo-bi.png';
 
 const Header = () => {
   return (
@@ -9,6 +13,16 @@ const Header = () => {
       <img src={logo} alt="Nexter logo" className={styles.logo} />
       <h3 className={font.heading3}>Your own home:</h3>
       <h3 className={font.heading1}>The ultimate personal freedom</h3>
+      <button className={`${font.btn} ${styles.btn}`}>
+        View our properties
+      </button>
+      <div className={styles.seenonText}>As seen on</div>
+      <div className={styles.seenonLogos}>
+        <img src={bbc} alt="Seen on logo 1" />
+        <img src={forbes} alt="Seen on logo 2" />
+        <img src={techcrunch} alt="Seen on logo 3" />
+        <img src={bi} alt="Seen on logo 4" />
+      </div>
     </header>
   );
 };
